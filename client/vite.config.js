1dbdecae144ddccc9bd0b061751950971fbdf03a
VP <veikko.pursiainen@aalto.fi>
2025-11-14 23:36:28 +0200

    Svelte for the fans

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,6 @@
-import { defineConfig } from "vite";
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+	plugins: [sveltekit()]
 });
